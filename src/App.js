724053aa744b0f84447995e7d6f86a472214093f
victor@victor <victor.dmaina@gmail.com>
2024-05-15 19:44:56 +0300

    complete texts

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 function App() {
-  const [pepperoniIsChecked, setPepperoniIsChecked] = useState(false);
+  const [ pepperoniIsChecked, setPepperoniIsChecked ] = useState(false);
 
-  function togglePepperoni(e) {
-    setPepperoniIsChecked(e.target.checked);
-  }
+  const togglePepperoni = (e) => setPepperoniIsChecked(e.target.checked);
 
   return (
     <div>
       <h1>Select Pizza Toppings</h1>
-      <input
-        type="checkbox"
-        id="pepperoni"
+      <input 
+        type="checkbox" 
+        id="pepperoni" 
         checked={pepperoniIsChecked}
         aria-checked={pepperoniIsChecked}
         onChange={togglePepperoni}
@@ -24,8 +22,8 @@ function App() {
         <li>Cheese</li>
         {pepperoniIsChecked ? <li>Pepperoni</li> : null}
       </ul>
+
     </div>
-  );
-}
+  )}
 
 export default App;
